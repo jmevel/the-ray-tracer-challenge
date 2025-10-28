9e9710246f7c2fc0c2f55a739765f1f514c19c0e
Jérôme MEVEL <2572775+jmevel@users.noreply.github.com>
2025-10-28 13:59:22 +0100

    test(matrices): 2x2 and 3x3

--- tests/matrices/matrices.feature
+++ tests/matrices/matrices.feature
@@ -13,3 +13,21 @@ Feature: Matrices
     And M[2,2] = 11
     And M[3,0] = 13.5
     And M[3,2] = 15.5
+
+  Scenario: A 2x2 matrix ought to be representable
+    Given the following 2x2 matrix M:
+      | -3 | 5  |
+      | 1  | -2 |
+    Then M[0,0] = -3
+    And M[0,1] = 5
+    And M[1,0] = 1
+    And M[1,1] = -2
+
+  Scenario: A 3x3 matrix ought to be representable
+    Given the following 3x3 matrix M:
+      | -3 |  5 |  0 |
+      |  1 | -2 | -7 |
+      |  0 |  1 |  1 |
+    Then M[0,0] = -3
+    And M[1,1] = -2
+    And M[2,2] = 1
--- tests/matrices/matrices_steps.rs
+++ tests/matrices/matrices_steps.rs
@@ -5,12 +5,28 @@ use the_ray_tracer_challenge::matrix::Matrix;
 
 #[derive(Debug, Default, World)]
 pub struct MatrixWorld {
+    matrices2x2: HashMap<String, Matrix<2, 2>>,
+    matrices3x3: HashMap<String, Matrix<3, 3>>,
     matrices4x4: HashMap<String, Matrix<4, 4>>,
+    matrices: HashMap<String, (usize, usize)>
+}
+
+#[given(expr = "the following 2x2 matrix {word}:")]
+fn the_following_2x2_matrix(world: &mut MatrixWorld, matrix: String, step: &Step) {
+    world.matrices2x2.insert(matrix.clone(), get_matrix::<2, 2>(step));
+    world.matrices.insert(matrix, (2,2));
+}
+
+#[given(expr = "the following 3x3 matrix {word}:")]
+fn the_following_3x3_matrix(world: &mut MatrixWorld, matrix: String, step: &Step) {
+    world.matrices3x3.insert(matrix.clone(), get_matrix::<3, 3>(step));
+    world.matrices.insert(matrix, (3,3));
 }
 
 #[given(expr = "the following 4x4 matrix {word}:")]
 fn the_following_4x4_matrix(world: &mut MatrixWorld, matrix: String, step: &Step) {
-    world.matrices4x4.insert(matrix, get_matrix4x4(step));
+    world.matrices4x4.insert(matrix.clone(), get_matrix::<4, 4>(step));
+    world.matrices.insert(matrix, (4,4));
 }
 
 #[then(expr = "{word}[{int},{int}] = {float}")]
@@ -21,13 +37,20 @@ fn matrix_equals(
     col_idx: usize,
     value: f32,
 ) {
-    assert_eq!(world.get_matrix4x4(matrix).data[row_idx][col_idx], value);
+    match world.matrices.get(&matrix)
+        .expect(format!("{matrix} does not exist").as_str())
+    {
+        (2,2) => assert_eq!(world.get_matrix2x2(matrix).data[row_idx][col_idx], value),
+        (3,3) => assert_eq!(world.get_matrix3x3(matrix).data[row_idx][col_idx], value),
+        (4,4) => assert_eq!(world.get_matrix4x4(matrix).data[row_idx][col_idx], value),
+        _ => panic!("no matrix with given size")
+    }
 }
 
-fn get_matrix4x4(step: &Step) -> Matrix<4, 4> {
-    let mut data: [[f32; 4]; 4] = Default::default();
+fn get_matrix<const ROW_COUNT: usize, const COL_COUNT: usize>(step: &Step) -> Matrix<ROW_COUNT, COL_COUNT> {
+    let mut data = Vec::<[f32; ROW_COUNT]>::with_capacity(COL_COUNT);
     let table = step.table.as_ref().unwrap();
-    for (row_idx, row_value) in table.rows.iter().enumerate().map(|(idx, row_values)| {
+    for (_, row_value) in table.rows.iter().enumerate().map(|(idx, row_values)| {
         (
             idx,
             row_values
@@ -36,13 +59,23 @@ fn get_matrix4x4(step: &Step) -> Matrix<4, 4> {
                 .collect::<Vec<f32>>(),
         )
     }) {
-        data[row_idx] = row_value.try_into().unwrap();
+        data.push(row_value.try_into().unwrap());
     }
 
-    Matrix { data }
+    Matrix { data: data.try_into().unwrap() }
 }
 
 impl MatrixWorld {
+    fn get_matrix2x2(&self, matrix: String) -> &Matrix<2, 2> {
+        self.matrices2x2
+            .get(&matrix)
+            .expect(format!("{matrix} does not exist").as_str())
+    }
+    fn get_matrix3x3(&self, matrix: String) -> &Matrix<3, 3> {
+        self.matrices3x3
+            .get(&matrix)
+            .expect(format!("{matrix} does not exist").as_str())
+    }
     fn get_matrix4x4(&self, matrix: String) -> &Matrix<4, 4> {
         self.matrices4x4
             .get(&matrix)
